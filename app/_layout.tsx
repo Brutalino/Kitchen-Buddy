8f4a95a9785970f7e9e671da44bb5151ab3ecc51
Fabio Marconi <fab.mar.2000@gmail.com>
2025-09-18 12:26:30 +0200

    Simplify root layout to resolve snack navigation conflict

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,11 @@
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { IngredientsProvider } from '../src/context/IngredientsContext';
 
 export default function RootLayout() {
   return (
-    <NavigationContainer>
-      <IngredientsProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="(screens)" options={{ headerShown: false }} />
-        </Stack>
-      </IngredientsProvider>
-    </NavigationContainer>
+    <IngredientsProvider>
+      <Slot />
+    </IngredientsProvider>
   );
 }
