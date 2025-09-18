75bea5136d09e0236eebd98bfb6615564cc8e0b4
Fabio Marconi <fab.mar.2000@gmail.com>
2025-09-18 12:21:36 +0200

    Fixed dependecies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,17 @@
 import { Stack } from 'expo-router';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { IngredientsProvider } from '../src/context/IngredientsContext';
 
 export default function RootLayout() {
   return (
-    // Spostiamo il Provider qui, al livello più alto possibile
-    <IngredientsProvider>
-      <Stack>
-        {/* Questa rotta si riferisce al nostro gruppo di schede (Tabs) */}
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
-        {/* Questa rotta si riferisce al nostro gruppo di schermate (Stack) */}
-        <Stack.Screen name="(screens)" options={{ headerShown: false }} />
-      </Stack>
-    </IngredientsProvider>
+    <NavigationContainer>
+      <IngredientsProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(screens)" options={{ headerShown: false }} />
+        </Stack>
+      </IngredientsProvider>
+    </NavigationContainer>
   );
 }
